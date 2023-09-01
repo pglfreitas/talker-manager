0b806617a9aa642f23e54f5eb7cd1001d2bd7e14
Pedro Gabriel <pglfreitas@gmail.com>
2023-09-01 17:25:34 -0300

    Conclusão req 1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 const express = require('express');
 
+const fs = require('fs').promises;
+const path = require('path');
+
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
-// não remova esse endpoint, e para o avaliador funciona
+// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+const talkerPath = path.resolve(__dirname, './talker.json');
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(talkerPath);
+    return JSON.parse(data);
+  } catch (erro) {
+    console.error(erro);
+  }
+};
+
+app.get('/talker', async (req, res) => {
+	try {
+	  const talker = await readFile();
+	  res.status(200).json(talker);
+	} catch (err) {
+	  res.status(200).send([]);
+	}
+  });
+
 
 app.listen(PORT, () => {
   console.log('Online');
