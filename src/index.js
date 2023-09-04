03a9216eb7d1a4a7c5a5404aa6771c71a6d04587
Pedro Gabriel <pglfreitas@gmail.com>
2023-09-04 17:29:16 -0300

    Conclusão req 6 e lint fix

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const path = require('path');
 
 const { emailValidation, passwordValidation } = require('./validations/validateLogin');
 const {
-	authorizationValidation,
-	nameValidation,
-	ageValidation,
-	talkValidation,
-	watchedValidation,
-	rateValidation,
+authorizationValidation,
+nameValidation,
+ageValidation,
+talkValidation,
+watchedValidation,
+rateValidation,
 } = require('./validations/validateTalker');
 
 const app = express();
@@ -36,40 +36,40 @@ const readFile = async () => {
 };
 
 const writeFile = async (data) => {
-	try {
-	  const response = await fs.writeFile(talkerPath, JSON.stringify(data));
-	  return response;
-	} catch (erro) {
-	  console.error(erro);
-	}
-  };
+try {
+const response = await fs.writeFile(talkerPath, JSON.stringify(data));
+return response;
+} catch (erro) {
+console.error(erro);
+}
+};
 
 app.get('/talker', async (req, res) => {
-    const talker = await readFile();
-      res.status(200).json(talker);
-    if (!talker) {
-      res.status(200).send([]);
-    }
-  });
+const talker = await readFile();
+res.status(200).json(talker);
+if (!talker) {
+res.status(200).send([]);
+}
+});
 
 app.get('/talker/:id', async (req, res) => {
-    const talker = await readFile();
-    const talkerId = talker.find(({ id }) => id === Number(req.params.id));
-    if (!talkerId) {
-        return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
-        }
-        return res.status(200).json(talkerId);
-    });
+const talker = await readFile();
+const talkerId = talker.find(({ id }) => id === Number(req.params.id));
+if (!talkerId) {
+return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+}
+return res.status(200).json(talkerId);
+});
 
 app.post('/login', emailValidation, passwordValidation, async (req, res) => {
-    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    const charLength = chars.length;
-    let result = '';
-    const length = 16;
-    for (let index = 0; index < length; index += 1) {
-       result += chars.charAt(Math.floor(Math.random() * charLength));
-    }
-     return res.status(200).json({ token: result });
+const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+const charLength = chars.length;
+let result = '';
+const length = 16;
+for (let index = 0; index < length; index += 1) {
+result += chars.charAt(Math.floor(Math.random() * charLength));
+}
+return res.status(200).json({ token: result });
 });
 
 app.post('/talker',
@@ -79,25 +79,49 @@ ageValidation,
 talkValidation,
 watchedValidation,
 rateValidation,
- async (req, res) => {
-	const { name, age, talk } = req.body;
-	const { watchedAt, rate } = talk;
-	const talker = await fs.readFile(talkerPath);
-	const talkerParse = JSON.parse(talker);
-	const id = talkerParse.length + 1;
-	const newTalker = { 
-		id,
-		name,
-		age,
-		talk: {
-		 watchedAt,
-		 rate,
-	} };
-	talkerParse.push(newTalker);
-	await writeFile(talkerParse);
-	res.status(201).send(newTalker);
-  });
+async (req, res) => {
+const { name, age, talk } = req.body;
+const { watchedAt, rate } = talk;
+const talker = await fs.readFile(talkerPath);
+const talkerParse = JSON.parse(talker);
+const id = talkerParse.length + 1;
+const newTalker = { 
+id,
+name,
+age,
+talk: {
+watchedAt,
+rate,
+} };
+talkerParse.push(newTalker);
+await writeFile(talkerParse);
+res.status(201).send(newTalker);
+});
+
+app.put('/talker/:id',
+authorizationValidation,
+nameValidation,
+ageValidation,
+talkValidation,
+watchedValidation,
+rateValidation,
+async (req, res) => {
+const { id } = req.params;
+const { name, age, talk } = req.body;
+const { watchedAt, rate } = talk;
+const talker = await fs.readFile(talkerPath);
+const talkerParse = JSON.parse(talker);
+const index = talkerParse.findIndex((person) => person.id === Number(id));
+if (index === -1) {
+return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+}
+const talkerUpdate = { name, age, id: Number(id), talk: { watchedAt, rate },
+};
+talkerParse[index] = talkerUpdate;
+await writeFile(talkerParse);
+res.status(200).json(talkerUpdate);
+});
 
 app.listen(PORT, () => {
-  console.log('Online');
+console.log('Online');
 });
--- src/validations/validateLogin.js
+++ src/validations/validateLogin.js
@@ -1,36 +1,36 @@
-	const emailValidation = async (req, res, next) => {
-	const { email } = req.body;
-	
-	const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+const emailValidation = async (req, res, next) => {
+const { email } = req.body;
 
-	if (!email) {
-	  return res.status(400).json(
-		{ message: 'O campo \"email\" é obrigatório' },
-	  );
-	}
-  
-	if (regex.test(email) === false) {
-	  return res.status(400).json(
-		{ message: 'O \"email\" deve ter o formato \"email@email.com\"' },
-	  );
-	}
-	next();
+const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+if (!email) {
+return res.status(400).json(
+{ message: 'O campo "email" é obrigatório' },
+);
+}
+
+if (regex.test(email) === false) {
+return res.status(400).json(
+{ message: 'O "email" deve ter o formato "email@email.com"' },
+);
+}
+next();
 };
 const passwordValidation = async (req, res, next) => {
-	const { password } = req.body;
+const { password } = req.body;
 
-	if (!password) {
-		return res.status(400).json(
-		  { message: 'O campo \"password\" é obrigatório' },
-		);
-	  }
-	
-	if (password.length < 6) {
-		return res.status(400).json(
-		  { message: 'O \"password\" deve ter pelo menos 6 caracteres' },
-		);
-	  }
-	next();
+if (!password) {
+return res.status(400).json(
+{ message: 'O campo "password" é obrigatório' },
+);
+}
+
+if (password.length < 6) {
+return res.status(400).json(
+{ message: 'O "password" deve ter pelo menos 6 caracteres' },
+);
+}
+next();
 };
 
-	module.exports = { emailValidation, passwordValidation };
+module.exports = { emailValidation, passwordValidation };
